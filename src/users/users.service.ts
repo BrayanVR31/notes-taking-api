800aa283d2826f7e0be0680f1847135a05fb8fb0
BrayanVR31 <brayan.vazquez.ramirez.2000@gmail.com>
2025-09-15 18:47:03 -0600

    add encryption to password user account

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma.service';
 import { Prisma, User } from '@prisma/client';
 import { CreateUserDto } from './dto/create-user.dto';
+import bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersService {
@@ -19,9 +20,13 @@ export class UsersService {
     return this.prisma.user.findMany(params);
   }
 
-  create(user: CreateUserDto) {
+  async create(user: CreateUserDto) {
+    const hashedPass = await bcrypt.hash(user.password, 10);
     return this.prisma.user.create({
-      data: user
+      data: {
+        ...user,
+        password: hashedPass,
+      }
     });
   }
 
